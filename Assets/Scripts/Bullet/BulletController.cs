49e5ae6880659619ba80f11424c192f84c53eac2
platelen <platelen@gmail.com>
2023-08-22 20:42:08 +0300

    Game_01
    Настроил управление у игрока от джойстика.
    Пули выстреливают по событию при нажатии на кнопке.

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -1,23 +1,24 @@
-using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Bullet
 {
     public class BulletController : MonoBehaviour
     {
-        [SerializeField] private float _speedBullet;
         [SerializeField] private float _damageBullet;
+        [SerializeField] private float _destroyDelay = 3f;
 
-        private void Update()
+        private void Start()
         {
-            MoveBullet();
+            StartCoroutine(DestroyObjectAfterDelay());
         }
 
-        private void MoveBullet()
+        private IEnumerator DestroyObjectAfterDelay()
         {
-            Rigidbody2D rb2D = GetComponent<Rigidbody2D>();
-            rb2D.velocity = new Vector2(_speedBullet, 0);
+            yield return new WaitForSeconds(_destroyDelay);
+            Destroy(gameObject);
         }
+
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.CompareTag("Player"))
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,22 +1,51 @@
-using Events;
 using UnityEngine;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private GameObject _bullet;
-        [SerializeField] private Transform _startBullet;
-        
-        
-        private void Awake()
+        [SerializeField] private float _speed = 10f;
+        [SerializeField] private float _rotationSpeed = 100f;
+        [SerializeField] private Joystick _joystick;
+
+        private Animation _anim;
+        private Vector2 _movement;
+        private Rigidbody2D _rb;
+
+
+        private void Start()
         {
-            GlobalEventsManager.OnStartBullet.AddListener(StartBullet);
+            _rb = GetComponent<Rigidbody2D>();
         }
 
-        private void StartBullet()
+        private void Update()
         {
-            Instantiate(_bullet, _startBullet.position, Quaternion.identity);
+            if (_joystick.Horizontal > 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+            if (_joystick.Horizontal < 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+
+            // if (_joystick.Horizontal != 0)
+            // {
+            //     _anim.Play();
+            // }
+            // else
+            // {
+            //     _anim.Stop();
+            // }
+
+            Vector2 moveInput = new Vector2(_joystick.Horizontal, _joystick.Vertical);
+            _movement = moveInput.normalized * _speed;
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.MovePosition(_rb.position + _movement * Time.deltaTime);
         }
     }
 }
